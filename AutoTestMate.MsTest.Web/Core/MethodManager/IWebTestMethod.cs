e3c7f4f1b11ef12aafd7da2c6a1ab40b1d0df04b
Josip Turcic <sportyturks@gmail.com>
2020-08-24 17:44:46 -0700

    Fix parallel bugs

---
+++ AutoTestMate.MsTest.Web/Core/MethodManager/IWebTestMethod.cs
@@ -0,0 +1,13 @@
+﻿using AutoTestMate.MsTest.Infrastructure.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AutoTestMate.MsTest.Web.Core.MethodManager
+{
+    public interface IWebTestMethod
+    {
+        IWebDriver WebDriver { get; set; }
+        WebDriverWait WebDriverWait { get; set; }
+        IFactory<IDriverCleanup> DriverCleanup { get; set; }
+    }
+}
